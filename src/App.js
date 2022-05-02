f9bb5802c47cee828e17b34ebd8826c2a5bd0542
Karim Mahfouz <kmahfouz@bsu.edu>
2022-05-02 14:18:41 -0400

    React Forum

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from "./Pages/LoginPage/LoginPage";
+import RegisterPage from "./Pages/RegisterPage/RegisterPage";
+import HomePage from "./Pages/HomePage/HomePage";
+import ThreadBox from './Pages/Threads/ThreadBox';
+import TopicPage from "./Pages/TopicPage/TopicPage";
+import {useState} from "react";
+import {
+    Routes,
+    Route,
+    BrowserRouter,
+} from "react-router-dom";
 
+// make function to grab token from local storage, make home page. at home page has useEFFECT, make api function called get topics then display
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<LoginPage/>} />
+                <Route path='/register' element={<RegisterPage/>}/>
+                <Route path='/home' element={<HomePage/>}/>
+                <Route path='home/topics/:topicId' element={<TopicPage/>}/>
+                <Route path="/threads/:threadId" element={<TopicPage/>}/>
+                {/*<Route path='/NewTopicWindow' element={<Threads/>}/>*/}
+            </Routes>
+        </BrowserRouter>
   );
 }
 
